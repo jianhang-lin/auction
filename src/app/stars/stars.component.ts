5fd829577fd488f314b8674c57caa1ee6bec38f9
JianhangLin <linjianhang1010@gmail.com>
2019-09-11 10:23:51 +0800

    feat(stars): define a @Input() rating field

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stars.component.css']
 })
 export class StarsComponent implements OnInit {
+
+  @Input()
+  private rating: number;
 
   private stars: string[];
 
