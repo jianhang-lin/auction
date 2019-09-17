55d063c82cae1ac475af42b296c5b79e225221aa
JianhangLin <linjianhang1010@gmail.com>
2019-09-17 23:53:17 +0800

    feat(start): add ratingChange as output property

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-stars',
@@ -9,6 +9,9 @@ export class StarsComponent implements OnInit {
 
   @Input()
   private rating: number;
+
+  @Output()
+  private ratingChange: EventEmitter<number> = new EventEmitter();
 
   private stars: string[];
 
