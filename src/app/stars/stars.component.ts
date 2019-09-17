76efd062a4a29049503335e950bdf21653579945
JianhangLin <linjianhang1010@gmail.com>
2019-09-18 00:13:14 +0800

    feat(start): init the star rating when value change

--- src/app/stars/stars.component.ts
+++ src/app/stars/stars.component.ts
@@ -1,11 +1,11 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import { Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-stars',
   templateUrl: './stars.component.html',
   styleUrls: ['./stars.component.css']
 })
-export class StarsComponent implements OnInit {
+export class StarsComponent implements OnInit, OnChanges{
 
   @Input()
   private rating: number;
@@ -21,17 +21,20 @@ export class StarsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    this.stars = [];
-    for (let i = 1; i <= 5; i++) {
-      this.stars.push(i > this.rating ? 'farstar' : 'fasstar');
-    }
+
   }
 
   clickStar(index: number) {
     if (!this.readonly) {
       this.rating = index + 1;
-      this.ngOnInit();
       this.ratingChange.emit(this.rating);
+    }
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.stars = [];
+    for (let i = 1; i <= 5; i++) {
+      this.stars.push(i > this.rating ? 'farstar' : 'fasstar');
     }
   }
 }
