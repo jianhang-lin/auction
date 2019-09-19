ffc6cf491e1f1634da9c17de97eeb05a452de4a7
JianhangLin <linjianhang1010@gmail.com>
2019-09-19 10:57:20 +0800

    feat(server): update websocket rgister

--- server/src/main/java/work/jianhang/server/config/WebSocketConfig.java
+++ server/src/main/java/work/jianhang/server/config/WebSocketConfig.java
@@ -13,7 +13,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
-        webSocketHandlerRegistry.addHandler(auctionHandler(), "actionHandler/{ID}");
+        webSocketHandlerRegistry.addHandler(auctionHandler(), "actionHandler/{ID}").setAllowedOrigins("*");
     }
 
     public WebSocketHandler auctionHandler() {
