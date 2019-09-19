6940b904420aaed292fa64c312224b9d5b263235
JianhangLin <linjianhang1010@gmail.com>
2019-09-19 15:37:44 +0800

    feat(server): session and productId put in the map

--- server/src/main/java/work/jianhang/server/handler/AuctionHandler.java
+++ server/src/main/java/work/jianhang/server/handler/AuctionHandler.java
@@ -7,16 +7,22 @@ import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Slf4j
 public class AuctionHandler extends TextWebSocketHandler {
 
+    private static Map<Object, List<Integer>> subscription = new HashMap<>();
+
     @Override
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
         String payload = message.getPayload();
-        Map<String, String> map = JSONObject.parseObject(payload, HashMap.class);
-        log.info("=====接收到的数据：{}", map);
+        Map<String, String> messageObj = JSONObject.parseObject(payload, HashMap.class);
+        log.info("=====接收到的数据：{}", messageObj);
+        List<Integer> productIds = subscription.get(session);
+        productIds.add(Integer.parseInt(messageObj.get("productId")));
+        subscription.put(session, productIds);
         session.sendMessage(new TextMessage("服务器返回收到的消息:" + payload));
     }
 }
