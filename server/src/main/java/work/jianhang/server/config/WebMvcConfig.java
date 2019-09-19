b15ff54e76efddb3a5385b46c2e2b972dc31b973
JianhangLin <linjianhang1010@gmail.com>
2019-09-19 12:47:22 +0800

    fix(server): fix the cros

---
+++ server/src/main/java/work/jianhang/server/config/WebMvcConfig.java
@@ -0,0 +1,24 @@
+package work.jianhang.server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * 处理跨域问题
+ * @author Grant Lin
+ * @date 2019-07-18
+ */
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("*")
+                .maxAge(3600);
+    }
+
+}
