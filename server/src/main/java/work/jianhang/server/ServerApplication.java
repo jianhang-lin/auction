283277af4dd434a2c8a26f12f9003ac94f39a732
JianhangLin <linjianhang1010@gmail.com>
2019-09-19 16:07:17 +0800

    refactor(server): init TaskScheduler bean

--- server/src/main/java/work/jianhang/server/ServerApplication.java
+++ server/src/main/java/work/jianhang/server/ServerApplication.java
@@ -2,7 +2,13 @@ package work.jianhang.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 
+@EnableScheduling
 @SpringBootApplication
 public class ServerApplication {
 
@@ -10,4 +16,11 @@ public class ServerApplication {
         SpringApplication.run(ServerApplication.class, args);
     }
 
+    @Bean
+    public TaskScheduler scheduledExecutorService() {
+        ThreadPoolTaskScheduler scheduler = new ThreadPoolTaskScheduler();
+        scheduler.setPoolSize(2);
+        scheduler.setThreadNamePrefix("scheduled-thread-");
+        return scheduler;
+    }
 }
