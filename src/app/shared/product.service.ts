62c80a483099629a3c51ac0709a0732707d90585
JianhangLin <linjianhang1010@gmail.com>
2019-09-19 14:05:30 +0800

    feat(product): add searchEvent property

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
@@ -9,6 +9,8 @@ import { catchError, tap } from 'rxjs/operators';
 export class ProductService {
 
   private baseUrl = 'http://localhost:8080';
+
+  searchEvent: EventEmitter<ProductSearchParams> = new EventEmitter();
 
   constructor(private http: HttpClient ) { }
 
